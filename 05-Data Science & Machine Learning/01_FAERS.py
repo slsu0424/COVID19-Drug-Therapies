1cde0b7e5e26fa1dfac2c3f66508ac008ac3eb85
slsu0424 <slsu0424>
2022-03-31 21:33:32 +0000

    update save to adls

--- 05-Data Science & Machine Learning/01_FAERS.py
+++ 05-Data Science & Machine Learning/01_FAERS.py
@@ -452,15 +452,21 @@ print((df2.count(), len(df2.columns)))
 
 # convert to pandas
 
-df3 = df2.toPandas()
+#df3 = df2.toPandas()
 
 #df2 = df1.toPandas()
 
 # COMMAND ----------
 
-df3.to_csv('/dbfs/mnt/adls/FAERS_CSteroid_preprocess1.csv', index=False)
+df2.write.csv('/dbfs/mnt/adls/FAERS_CSteroid_preprocess1.csv')
 
 #df2.to_csv('/dbfs/mnt/adls/FAERS_CSteroid_preprocess1.csv', index=False)
+
+#df2.to_csv('/dbfs/mnt/adls/FAERS_CSteroid_preprocess1.csv', index=False)
+
+# COMMAND ----------
+
+df2.write.parquet('/dbfs/mnt/adls/FAERS_CSteroid_preprocess1.parquet')
 
 # COMMAND ----------
 
