83ec5cbeca7d845f4c8b357044549befda26702c
slsu0424 <sandylsu@gmail.com>
2022-07-20 22:41:33 -0400

    Update __init__.py

--- 03-Data Engineering/Azure Functions/HttpTrigger_runFunctionFromADF/__init__.py
+++ 03-Data Engineering/Azure Functions/HttpTrigger_runFunctionFromADF/__init__.py
@@ -1,12 +1,12 @@
-from unittest import skip
 import azure.functions as func
 import json
 import logging
 import io
 import zipfile
-#import pandas as pd
+import pandas as pd
 from azure.storage.blob import BlobServiceClient
 from azure.core.exceptions import ResourceNotFoundError
+from unittest import skip
 
 
 def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
@@ -25,13 +25,11 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
     # set input blob
     blob_client = blob_service_client.get_blob_client(container="raw/FAERS", blob = inputFileName)
 
-    # set output container1
+    # set output containers
     container_client1 = blob_service_client.get_container_client("curated/FAERS_output_txt_AZFunction")
+    container_client2 = blob_service_client.get_container_client("curated/FAERS_output_txt_AZFunction_parquet")
 
-    # set output container2
-    container_client2 = blob_service_client.get_container_client("curated/FAERS_output_txt_parquet")
-
-    # download input blob as a stream
+    # download input blob as stream
     with io.BytesIO() as b:
         try:
             download_stream = blob_client.download_blob()
@@ -41,24 +39,19 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
 
         # open stream in READ mode
         with zipfile.ZipFile(b) as zip:
-        
-            # print all contents of the zip file
+ 
             logging.info(f"List of contents: {zip.namelist()}")
-            
-            # extract to output container
             logging.info(f"Extracting files now...")
 
             for filename in zip.namelist():
-            
-                # 1) Copy txt files to target
-                logging.info(f"Extracting files now...")
+
                 logging.info(f"Extracting file: {filename}")
 
                 if filename.endswith('.TXT'):
 
                     with zip.open(filename, mode='r') as f:
                     
-                        # name of file to be landed in the target container
+                        # 1) copy txt files -> load to target
                         if '/' in filename:
                             filename_target = filename.split('/', 1)[1]
                         else:
@@ -66,6 +59,20 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
                         
                         container_client1.get_blob_client(filename_target).upload_blob(f)
                 
+                        # 2) read txt files and convert to parquet -> load to target
+                        filename_target2 = filename_target.replace('.TXT', '.parquet', 1)
+                        
+                        filetoread = io.BytesIO(zip.read(filename))
+
+                        logging.info(f"filetoread is: {filetoread}")
+
+                        df = pd.read_csv(filetoread, sep='$')
+
+                        parquet_file = io.BytesIO()
+                        df.to_parquet(parquet_file)
+                        parquet_file.seek(0)
+
+                        container_client2.get_blob_client(filename_target2).upload_blob(parquet_file)
 
                 else:
                     # do not process any non .txt/TXT files 
