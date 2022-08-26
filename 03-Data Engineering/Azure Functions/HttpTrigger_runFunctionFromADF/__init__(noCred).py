896818bae1e1a6b9fbae4af345095d62ac6fc66b
slsu0424 <sandylsu@gmail.com>
2022-08-26 16:21:02 -0400

    updated file

--- 03-Data Engineering/Azure Functions/HttpTrigger_runFunctionFromADF/__init__(noCred).py
+++ 03-Data Engineering/Azure Functions/HttpTrigger_runFunctionFromADF/__init__(noCred).py
@@ -1,3 +1,4 @@
+from chunk import Chunk
 import azure.functions as func
 import json
 import logging
@@ -19,7 +20,7 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
     logging.info(f"Request received to extract file: {inputFileName}")
 
     # connect to storage account
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=asastgssuaefdbhdg2dbc4;AccountKey=/wDoU7XVXzCZbcVE8XjYXVfwVWVtKkUK38nFG9MhP2Vg4JEVX1j7Wws43b9NnzctMf2nGFtx8dhw+AStw3YRPA==;EndpointSuffix=core.windows.net"
+    connection_string = "<enter your connection string>"
     blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
 
     # set input blob
@@ -33,7 +34,15 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
     with io.BytesIO() as b:
         try:
             download_stream = blob_client.download_blob()
-            download_stream.readinto(b)
+
+            # check chunk size
+            for chunk in download_stream.chunks():
+                logging.info(f"chunk size is: {len(chunk)}") 
+
+            #download_stream.readinto(b)
+
+            download_stream.chunks.readinto(b)
+        
         except ResourceNotFoundError:
             logging.info(f"No blob found")
 
@@ -45,9 +54,9 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
 
             for filename in zip.namelist():
 
-                logging.info(f"Extracting file: {filename}")
-
-                if filename.endswith('.TXT'):
+                logging.info(f"Extracting file name: {filename}")
+                
+                if filename.endswith('.TXT') or filename.endswith('.txt'):
 
                     with zip.open(filename, mode='r') as f:
                     
@@ -59,20 +68,23 @@ def main(req: func.HttpRequest, obj: func.InputStream) -> func.HttpResponse:
                         
                         container_client1.get_blob_client(filename_target).upload_blob(f, overwrite = True)
                 
-                        # 2) convert txt to parquet and load to target
-                        filename_target2 = filename_target.replace('.TXT', '.parquet', 1)
-                        
-                        filetoread = io.BytesIO(zip.read(filename))
+#                        # 2) convert txt to parquet and load to target
+#                        filename_target2 = filename_target.replace('.TXT', '.parquet', 1)
+#                        
+#                        filetoread = io.BytesIO(zip.read(filename))
+#
+#                        logging.info(f"filetoread is: {filetoread}")
+#
+#                        df = pd.read_csv(filetoread, sep='$', dtype = {'I_F_COD': 'str'})
+#
+#                        DataType = df.dtypes
+#                        logging.info(f"Data Type is: {DataType}")
 
-                        logging.info(f"filetoread is: {filetoread}")
+#                        parquet_file = io.BytesIO()
+#                        df.to_parquet(parquet_file)
+#                        parquet_file.seek(0)
 
-                        df = pd.read_csv(filetoread, sep='$')
-
-                        parquet_file = io.BytesIO()
-                        df.to_parquet(parquet_file)
-                        parquet_file.seek(0)
-
-                        container_client2.get_blob_client(filename_target2).upload_blob(parquet_file, overwrite = True)
+#                        container_client2.get_blob_client(filename_target2).upload_blob(parquet_file, overwrite = True)
 
                 else:
                     # do not process any non .txt/TXT files 
